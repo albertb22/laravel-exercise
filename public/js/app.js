bb71de219642972eab9c2d8788465cbf121b7ecd
Robert-John van Doesburg <robertjohn.van.doesburg@guapa.nl>
2019-01-24 16:12:42 +0100

    Add message block

--- public/js/app.js
+++ public/js/app.js
@@ -1859,6 +1859,14 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: {
     activeChannel: {
@@ -36928,7 +36936,47 @@ var render = function() {
           )
         }),
         0
-      )
+      ),
+      _vm._v(" "),
+      _c("div", { staticClass: "pb-6 px-4 flex-none" }, [
+        _c(
+          "div",
+          {
+            staticClass: "flex rounded-lg border-2 border-grey overflow-hidden"
+          },
+          [
+            _c(
+              "span",
+              { staticClass: "text-3xl text-grey border-r-2 border-grey p-2" },
+              [
+                _c(
+                  "svg",
+                  {
+                    staticClass: "fill-current h-6 w-6 block",
+                    attrs: {
+                      xmlns: "http://www.w3.org/2000/svg",
+                      viewBox: "0 0 20 20"
+                    }
+                  },
+                  [
+                    _c("path", {
+                      attrs: {
+                        d:
+                          "M16 10c0 .553-.048 1-.601 1H11v4.399c0 .552-.447.601-1 .601-.553 0-1-.049-1-.601V11H4.601C4.049 11 4 10.553 4 10c0-.553.049-1 .601-1H9V4.601C9 4.048 9.447 4 10 4c.553 0 1 .048 1 .601V9h4.399c.553 0 .601.447.601 1z"
+                      }
+                    })
+                  ]
+                )
+              ]
+            ),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "w-full px-4",
+              attrs: { type: "text", placeholder: "Message #general" }
+            })
+          ]
+        )
+      ])
     ]
   )
 }
